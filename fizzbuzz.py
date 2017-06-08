ccea50aaa3f69d69adc48681cfb96b1def9d2ff5
medvesekg <gregor_medvesek@yahoo.com>
2017-06-08 11:17:41 +0200

    Initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+userNumber = raw_input("Select a number between 1 and 100: ")
+
+for i in range(1, int(userNumber)):
+    if i % 15 == 0:
+        print "fizzbuzz"
+    if i % 3 == 0:
+        print "fizz"
+    if i % 5 == 0:
+        print "buzz"
+    else:
+        print i
