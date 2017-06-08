9e0fcf32cbd39021e3327a634a8c74bf8c74a347
medvesekg <gregor_medvesek@yahoo.com>
2017-06-08 11:36:58 +0200

    Fixed elif

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,9 +3,9 @@ userNumber = raw_input("Select a number between 1 and 100: ")
 for i in range(1, int(userNumber)):
     if i % 15 == 0:
         print "fizzbuzz"
-    if i % 3 == 0:
+    elif i % 3 == 0:
         print "fizz"
-    if i % 5 == 0:
+    elif i % 5 == 0:
         print "buzz"
     else:
         print i
